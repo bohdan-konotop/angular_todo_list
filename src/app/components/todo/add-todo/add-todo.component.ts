b49a54f3277c7c0dc736be9cf59b14ef18a7fd87
Bogdan Konotop <bogdankonotop@Bogdans-MacBook-Air.local>
2022-05-30 16:38:47 +0300

    Rewrite on FormBuilder and add styles for completed text

--- src/app/components/todo/add-todo/add-todo.component.ts
+++ src/app/components/todo/add-todo/add-todo.component.ts
@@ -1,5 +1,5 @@
 import { Component, Output, EventEmitter } from '@angular/core';
-import {FormControl} from "@angular/forms";
+import { Validators, FormBuilder, FormGroup } from "@angular/forms";
 
 @Component({
   selector: 'app-add-todo',
@@ -7,13 +7,21 @@ import {FormControl} from "@angular/forms";
   styleUrls: ['./add-todo.component.css', '../styles/base.css']
 })
 export class AddTodoComponent {
-  todo = new FormControl('');
+  todo: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.todo = formBuilder.group({
+      "text": [null, [Validators.required, Validators.minLength(1), Validators.maxLength(65)]]
+    })
+  }
 
   @Output() newTodoEvent = new EventEmitter<string>();
 
   onSubmit() {
-    if(this.todo.value.length > 0) this.newTodoEvent.emit(this.todo.value);
+    if(this.todo.value.text === null) return;
+    this.newTodoEvent.emit(this.todo.value.text);
     this.todo.reset();
   }
+
 }
 
